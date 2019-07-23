54690bcb03cfae00133028d9b2146000b89a397b
Ian White <ibwhite@gmail.com>
2019-07-22 23:13:35 -0400

    use official @slack/webhook package instead of https post

--- clubhouse-webhook/index.js
+++ clubhouse-webhook/index.js
@@ -1,6 +1,6 @@
 /*global await*/
-const https = require("https");
 const Clubhouse = require("clubhouse-lib");
+const IncomingWebhook = require("@slack/webhook").IncomingWebhook;
 
 exports.handler = async (event, context, callback) => {
   console.log("Received event: " + JSON.stringify(event));
@@ -43,27 +43,14 @@ exports.handler = async (event, context, callback) => {
 
   if (payloadText) {
     console.log("Sending Slack message: " + payloadText);
-    const payload = JSON.stringify({
+    const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+    const payload = {
       text: payloadText,
       channel: process.env.SLACK_CHANNEL,
       icon_url:
         "https://avatars.slack-edge.com/2018-11-01/469021388864_b4432f702d54503e0e03_192.png",
       username: "Clubhouse"
-    });
-
-    const options = {
-      hostname: "hooks.slack.com",
-      method: "POST",
-      path: process.env.SLACK_WEBHOOK_URL.substring(
-        "https://hooks.slack.com".length
-      )
     };
-
-    const req = https.request(options, res =>
-      res.on("data", () => callback(null, "OK"))
-    );
-    req.on("error", error => callback(JSON.stringify(error)));
-    req.write(payload);
-    req.end();
+    await webhook.send(payload);
   }
 };
